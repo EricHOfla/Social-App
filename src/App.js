5e652cabff3d57a32eab26e4fb68d2c95d29b9ee
HABUMUGISHA Eric <ericofla1@gmail.com>
2022-02-10 15:39:08 +0200

    inition

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Profle from "./pages/profle/Profle";
+// import Register from "./pages/register/Register";
+// import Data from "./Data";
+
+function App() {
+  return(
+    <>
+     <Home />
+     {/* <Data /> */}
+     </>
+
+  );
+
+    
+  
+}
+
+export default App;
